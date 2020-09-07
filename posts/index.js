7e9dd2aeea71f2d67a9a67cc8afff29224cdfb5c
luckpp <marius.andritoi@yahoo.com>
2020-09-07 04:13:55 -0700

    update deployments

--- posts/index.js
+++ posts/index.js
@@ -17,7 +17,7 @@ app.get('/posts', (req, res) => {
 app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
-  
+
   posts[id] = {
     id,
     title
@@ -30,16 +30,17 @@ app.post('/posts', async (req, res) => {
       title
     }
   });
-  
+
   res.status(201).send(posts[id]);
 });
 
 app.post('/events', (req, res) => {
   console.log('Received Event:', req.body.type);
-  
+
   res.send({});
 });
 
 app.listen(4000, () => {
+  console.log('latest from docker hub');
   console.log('Listening on port 4000');
 });
