2d916c08599e888a72bb69f13d130ba34fa664ed
luckpp <marius.andritoi@yahoo.com>
2020-09-08 09:31:26 -0700

    connected the posts and event-bus microservices

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,10 +12,10 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
-  axios.post('http://localhost:4003/events', event);
+  axios.post('http://posts-clusterip-srv:4000/events', event);
+  // axios.post('http://localhost:4001/events', event);
+  // axios.post('http://localhost:4002/events', event);
+  // axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
 });
@@ -25,5 +25,5 @@ app.get('/events', (req, res) => {
 })
 
 app.listen(4005, () => {
-  console.log('[event-bus] Listening on port 4005');
+  console.log('[event-bus] Listening on port 4005!');
 });
